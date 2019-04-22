43b704d308ad7db389b6945883b6e00c15f24ab6
Oresti Duro <oduro@worcester.edu>
2019-04-22 08:41:07 -0400

    CD-Exercise

--- src/main/java/CS_348_SP_2019/CDExercise/EndsWith6.java
+++ src/main/java/CS_348_SP_2019/CDExercise/EndsWith6.java
@@ -5,10 +5,10 @@ public class EndsWith6 {
 	String names;
 
 	public EndsWith6()  {
-		names = "";
+		names = "Oresti Duro,";
 		// add your name here in the form of a line that looks like:
 		// names+= "John Smith, ";
-		
+
 	}
 
 	@Override
